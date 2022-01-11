04721bd630c2762d447e8b105c8c3921cb0d9386
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2022-01-11 11:35:34 +0100

    ajout elk.tf

--- elk.tf
+++ elk.tf
@@ -3,10 +3,14 @@ resource "aws_elasticsearch_domain" "projet10" {
     elasticsearch_version = "7.10"
   
     cluster_config {
-      instance_type = "r4.large.elasticsearch"
+      instance_type = "t3.small.elasticsearch"
     }
-  
+    ebs_options {
+        ebs_enabled = true
+        volume_size = 10
+        volume_type = "gp2"
+      }
     tags = {
       Domain = "projet10"
     }
-  }
+}
