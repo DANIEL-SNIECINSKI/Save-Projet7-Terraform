6a6fefbb0fa87b45871982e95b39bbf56f6f70c7
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2022-01-14 11:14:00 +0100

    update elk.tf

--- elk.tf
+++ elk.tf
@@ -10,6 +10,24 @@ resource "aws_elasticsearch_domain" "projet10" {
         volume_size = 10
         volume_type = "gp2"
       }
+#########################################
+access_policies = <<CONFIG
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "es:*",
+            "Principal": "*",
+            "Effect": "Allow",
+            "Resource": "arn:aws:es:eu-west-3:733179081070:domain/projet10/*"
+        }
+    ]
+  }
+CONFIG
+
+################################################
+
+
     tags = {
       Domain = "projet10"
     }
