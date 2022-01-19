d03c6c09e19512ec6d01e6d8cb3225e8c7797883
DANIEL-SNIECINSKI <daniel.sniecinski@wanadoo.fr>
2022-01-19 10:14:12 +0100

    update elk.tf-190122

--- elk.tf
+++ elk.tf
@@ -1,34 +1,52 @@
 resource "aws_elasticsearch_domain" "projet10" {
-    domain_name           = "projet10"
-    elasticsearch_version = "7.10"
+  domain_name           = "projet10"
+  elasticsearch_version = "OpenSearch_1.0"
   
-    cluster_config {
-      instance_type = "t3.small.elasticsearch"
-    }
-    ebs_options {
-        ebs_enabled = true
-        volume_size = 10
-        volume_type = "gp2"
-      }
-#########################################
-access_policies = <<CONFIG
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "es:*",
-            "Principal": "*",
-            "Effect": "Allow",
-            "Resource": "arn:aws:es:eu-west-3:733179081070:domain/projet10/*"
-        }
-    ]
+  cluster_config {
+    dedicated_master_enabled = false
+
+    instance_type = "t3.small.elasticsearch"
+    instance_count = 1
+
+    warm_enabled = false
   }
-CONFIG
 
-################################################
+  domain_endpoint_options {
+    enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
+  }
 
+  node_to_node_encryption {
+    enabled = true
+  }
 
-    tags = {
-      Domain = "projet10"
-    }
+  encrypt_at_rest {
+    enabled = true
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp2"
+    volume_size = 20
+  }
+
+  
+  access_policies = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "es:*"
+        ]
+        Principal = {
+          AWS = "*"
+        }
+        Effect    = "Allow"
+        Resource  = "*"
+      },
+    ]
+  })
 }
+
+
+
